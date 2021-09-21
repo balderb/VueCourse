3175368e5033fc95b92ec25c068e92149444af7c
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2021-09-21 19:28:50 +0200

    busy debugging apache server start

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var vm = vue.createApp({
+var vm = vue.createApp{
     data() {
         return {information: 'Vue works from the data object'} 
-}).mount('#app')
+})
+.mount('#app')
 // Mount = to insert a Vue application into the document
 
